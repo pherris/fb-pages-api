3cba069ae6036f9444feda71bf5e3d047853e7a7
Andrew Ralston <aralston@gmail.com>
2014-04-28 14:02:51 -0500

    new configuration options

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var config    = require( __dirname + '/config.js' );
 
 config.validate();
 
+console.log(JSON.stringify(config));
+
 var server    = restify.createServer();
 
 //allow cross origin for dev
@@ -23,7 +25,7 @@ function isEmptyObject(obj) {
   }
 }
 
-var myCache = new NodeCache({ stdTTL: 300, checkperiod: 120 } );
+var myCache = new NodeCache({ stdTTL: config.app.cache.ttlMillis, checkperiod: 120 } );
 
 // Creates a JSON client
 var fbClient = restify.createJsonClient({
@@ -36,37 +38,49 @@ var fbClient = restify.createJsonClient({
 server.get(/^\/fb(.*)/, function(req, resp, next) {
   var apiCall = req.params[0].substr(1, req.params[0].length); //pull off leading /
 
+  //if apiCall is numeric&/or hyphens, don't prepend pageId
+  if (!apiCall.match(/^[\d|-]+\/?.*$/)) {
+    apiCall = config.fb.pageId + '/' + apiCall;
+  }
+
   myCache.get( apiCall, function( err, value ){
     var seperator = apiCall.indexOf('?') > -1 ? '&' : '?';
+    
     if( isEmptyObject(value[apiCall] ) || err ){
       // no key in cache, populate
-      console.log('cache not found, fetching');
-
-      //if apiCall is numeric&/or hyphens, don't prepend pageId
-      if (!apiCall.match(/^[\d|-]+\/?.*$/)) {
-        apiCall = config.fb.pageId + '/' + apiCall;
-      }
+      //console.log('cache not found, fetching');
 
       var url = '/' + apiCall + seperator + 'access_token=' + config.fb.appId + '|' + config.fb.appSecret;
 
       fbClient.get(url, function(err, req, res, obj) {
-        console.log( 'fb call executed to: ' + url );
         console.log( 'fb response recevied' );
+        
         myCache.set(apiCall, obj);
         resp.send( obj ? obj : "{ 'error': 'could not access fb' }" );
         return next();
       });
     } else {
-      console.log( 'found cached value, returning' );
+      //console.log( 'found cached value, returning' );
       resp.send( value[apiCall] );
       return next();
     }
   });
+
+  return next();
 });
 
+//wrap the config object and only expose what is safe to expose
 server.get(/^\/config/, function (req, resp, next) {
   resp.send({
-    'appId': config.fb.appId
+    'facebook': {
+      'appId': config.fb.appId,
+      'pages': config.fb.pages
+    },
+    'google': {
+      'analytics': {
+        'siteId': config.google.analytics.siteId
+      }
+    }
   });
   return next();
 });
